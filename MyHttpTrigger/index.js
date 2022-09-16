9d4010606ecda263ffbc38b5fc4c545f05eace89
juriStudent <113450135+juriStudent@users.noreply.github.com>
2022-09-16 16:49:36 +0200

    Cosmos can now give and also take input.

--- MyHttpTrigger/index.js
+++ MyHttpTrigger/index.js
@@ -1,89 +1,68 @@
 // CONFIG ---- Start with "http(s)://"" and end with a "/".
-const URLDOMAIN = "http://127.0.0.1:5500/"
+const URLDOMAIN = "http://127.0.0.1:5500/";
 let MASTERPASS = "ikbenadmin";
 // CONFIG ----
 
 
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-}
+module.exports = async function (context, req, workersIN) {
+    workersIN = workersIN[0];
 
-// EXAMPLE DB
-let worker = {
-    name: "Cedric",
-    vehicleDescription: "ABC123",
-    km: 1000,
-    sessionID: "XYZZZZ"
-}
-// EXAMPLE DB
+    console.log("here");
+    let surName = context.bindingData.surName;
+    let lastName = context.bindingData.lastName;
+    let vehicleCode = context.bindingData.vehicleCode;
+    let vehicleDescription = context.bindingData.vehicleDescription;
+    let km = context.bindingData.km;
+    let transactionID = context.bindingData.transactionID;
 
-module.exports = async function (context, req, workers) {
-    
-    let test = true
-    if (test)
-    {
-        context.res = {body: workers};
-        return
+    // admin wants to generate a link for the target.
+    if (transactionID == MASTERPASS) {
+        let url = generateLink(workersIN, surName, lastName, vehicleCode, vehicleDescription, context);
+        context.res = {
+            body: url
+        };
     }
-
-
-    return __awaiter(this, void 0, void 0, function* () {
-        var name = context.bindingData.name;
-        var vehicleDescription = context.bindingData.vehicleDescription;
-        var km = context.bindingData.km;
-        var sessionID = context.bindingData.sessionID;
-        // admin wants to generate a link for the target.
-        if (sessionID == MASTERPASS) {
-            let url = generateLink(name, vehicleDescription, km);
-            context.res = {
-                body: url
-            };
-        }
-        // Target is trying to save his km.
-        else {
-            // Search for the worker's name and get his values.
-            if (worker.sessionID == sessionID) {
-                // update the database, remove the sessionID.
-                // get the km.
-                if (km < worker.km) {
-                    // The value that the user has given is lower than what's in the database.
-                    var res = { body: `<meta http-equiv=\"refresh\" content=\"0; url=${URLDOMAIN}?${worker.name}?${worker.vehicleDescription}?${worker.km}?${sessionID}?low" />`, headers: { "Content-Type": "text/html" } }
-                    context.res = res;
-                }
-                else {
-                    worker.sessionID = randomCodeGenerator(10);
-                    // SQL update workers km
-                    worker.km = km;
-
-                    // The value that the user has given is ok!
-                    var res = { body: `<meta http-equiv=\"refresh\" content=\"0; url=${URLDOMAIN}?${worker.name}?${worker.vehicleDescription}?${worker.km}?${sessionID}?ok" />`, headers: { "Content-Type": "text/html" } }
-                    context.res = res;
-                }
+    // Target is trying to save his km.
+    else {
+        // Search for the worker's name and get his values.
+        if (workersIN.transactionID == transactionID) {
+            // update the database, remove the transactionID.
+            // get the km.
+            if (km < workersIN.km) {
+                // The value that the user has given is lower than what's in the database.
+                var res = { body: `<meta http-equiv=\"refresh\" content=\"0; url=${URLDOMAIN}?${workersIN.name}?${workersIN.vehicleDescription}?${workersIN.km}?${transactionID}?low" />`, headers: { "Content-Type": "text/html" } };
+                context.res = res;
             }
             else {
-                context.res = {
-                    body: "INVALID sessionID"
-                };
-                // User's sessionID is not valid. Tell him his link is invalid.
+                workersIN.transactionID = randomCodeGenerator(10);
+                // SQL update worker's km
+                workersIN.km = km;
+
+                // The value that the user has given is ok!
+                var res = { body: `<meta http-equiv=\"refresh\" content=\"0; url=${URLDOMAIN}?${workersIN.name}?${workersIN.vehicleDescription}?${workersIN.km}?${transactionID}?ok" />`, headers: { "Content-Type": "text/html" } };
+                context.res = res;
             }
         }
-    });
-}
+        else {
+            context.res = {
+                body: "INVALID transactionID"
+            };
+            // User's transactionID is not valid. Tell him his link is invalid.
+        }
+    }
+};
 
-function generateLink(name, vehicleDescription, km) {
-    let sessionID = randomCodeGenerator(10);
-    //Search for the worker's name and get his values.
-    //change the allowed sessionID.
-    worker.sessionID = sessionID;
+function generateLink(workersIN, surName, lastName, vehicleCode, vehicleDescription, context) {
+    workersIN.surName = surName;
+    workersIN.lastName = lastName;
+    workersIN.vehicleCode = vehicleCode;
+    workersIN.vehicleDescription = vehicleDescription;
+    workersIN.transactionID = randomCodeGenerator(10);
+
+    updateWorker(workersIN, context);
 
     // INDEX SITE COMES HERE
-    let url = `${URLDOMAIN}?${name}?${worker.vehicleDescription}?${worker.km}?${sessionID}`;
+    let url = `${URLDOMAIN}?${surName}?${lastName}?${vehicleCode}?${workersIN.km}?${vehicleDescription}?${workersIN.transactionID}`;
     return url;
 }
 
@@ -95,4 +74,10 @@ function randomCodeGenerator(length) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
+}
+
+
+// make the values 'undefined' if you don't want them changed.
+function updateWorker(workersIN, context) {
+    context.bindings.workersOUT = JSON.stringify(workersIN);
 }
