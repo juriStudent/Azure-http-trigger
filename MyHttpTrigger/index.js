e18023c689c73d2b416203c8243b077127e44d3c
nNoidea <58337790+nNoidea@users.noreply.github.com>
2022-09-24 21:47:48 +0200

    Changed surName to firstName 😄

--- MyHttpTrigger/index.js
+++ MyHttpTrigger/index.js
@@ -5,15 +5,15 @@ let MASTERPASS = "TOP_MEGA_ULTRA_SECRET";
 
 
 module.exports = async function (context, req, databaseIN) {
-    let lastKM = 0
-    if (databaseIN.length != 0){
+    let lastKM = 0;
+    if (databaseIN.length != 0) {
         databaseIN = databaseIN[0];
         lastKM = databaseIN.km;
     }
     // Otherwise lastKM stays 0, because the vehicle did not exist untill now.
-    
+
     // Get the url parameters.
-    let surName = (context.bindingData.surName).toLowerCase();
+    let firstName = (context.bindingData.firstName).toLowerCase();
     let lastName = context.bindingData.lastName.toLowerCase();
     let vehicleCode = context.bindingData.vehicleCode;
     let vehicleDescription = context.bindingData.vehicleDescription;
@@ -26,22 +26,22 @@ module.exports = async function (context, req, databaseIN) {
     if (transactionID == MASTERPASS) {
 
         // If the entry is first, replace the value with the desired km.
-        if (lastKM == 0){
-            lastKM = km
+        if (lastKM == 0) {
+            lastKM = km;
         }
 
         // Fill in the template for the current worker.
         // Since the id is not given, Cosmos DB will generate a random unique one for the worker.
         databaseIN = {
             "timeID": timeID,
-            "surName": surName,
+            "firstName": firstName,
             "lastName": lastName,
             "vehicleCode": vehicleCode,
             "km": lastKM,
             "vehicleDescription": vehicleDescription,
             "transactionID": randomCodeGenerator(10),
             "status": "new"
-        }
+        };
 
         let url = generateLink(databaseIN, vehicleDescription, context);
         context.res = {
@@ -49,45 +49,45 @@ module.exports = async function (context, req, databaseIN) {
         };
     }
     // Target is trying to save his km.
-    else if (databaseIN.status =="new" && databaseIN.transactionID == transactionID) {
+    else if (databaseIN.status == "new" && databaseIN.transactionID == transactionID) {
         // The km input is valid, update it.
         if (km > lastKM) {
             // update the user, his transactionID gets tagged as "EXPIRED".
             databaseIN = {
-            "id":databaseIN.id,
-            "timeID": timeID,
-            "surName": databaseIN.surName,
-            "lastName": databaseIN.lastName,
-            "vehicleCode": databaseIN.vehicleCode,
-            "km": km,
-            "transactionID": "EXPIRED",
-            "vehicleDescription": databaseIN.vehicleDescription,
-            "status": "submitted"
-        }
+                "id": databaseIN.id,
+                "timeID": timeID,
+                "firstName": databaseIN.firstName,
+                "lastName": databaseIN.lastName,
+                "vehicleCode": databaseIN.vehicleCode,
+                "km": km,
+                "transactionID": "EXPIRED",
+                "vehicleDescription": databaseIN.vehicleDescription,
+                "status": "submitted"
+            };
 
-        addWorker(databaseIN, context)
+            addWorker(databaseIN, context);
 
-        // The value that the user has given is ok!
-        context.res = { body: `<meta http-equiv=\"refresh\" content=\"0; url=${URLDOMAIN}?${databaseIN.surName}?${databaseIN.lastName}?${databaseIN.vehicleCode}?${databaseIN.km}?${databaseIN.transactionID}?${databaseIN.vehicleDescription}?ok" />`, headers: { "Content-Type": "text/html" } };
+            // The value that the user has given is ok!
+            context.res = { body: `<meta http-equiv=\"refresh\" content=\"0; url=${URLDOMAIN}?${databaseIN.firstName}?${databaseIN.lastName}?${databaseIN.vehicleCode}?${databaseIN.km}?${databaseIN.transactionID}?${databaseIN.vehicleDescription}?ok" />`, headers: { "Content-Type": "text/html" } };
         }
         else {
             // The value that the user has given is lower than what's in the database.
-            context.res = { body: `<meta http-equiv=\"refresh\" content=\"0; url=${URLDOMAIN}?${databaseIN.surName}?${databaseIN.lastName}?${databaseIN.vehicleCode}?${databaseIN.km}?${databaseIN.transactionID}?${databaseIN.vehicleDescription}?low" />`, headers: { "Content-Type": "text/html" } };
+            context.res = { body: `<meta http-equiv=\"refresh\" content=\"0; url=${URLDOMAIN}?${databaseIN.firstName}?${databaseIN.lastName}?${databaseIN.vehicleCode}?${databaseIN.km}?${databaseIN.transactionID}?${databaseIN.vehicleDescription}?low" />`, headers: { "Content-Type": "text/html" } };
         }
-    } 
+    }
     else {
         context.res = {
             body: "INVALID transactionID"
         };
     }
-}
+};
 
 function generateLink(databaseIN, vehicleDescription, context) {
-   
+
     addWorker(databaseIN, context);
-    
+
     // URL FOR THE WEBPAGE.
-    let url = `${URLDOMAIN}?${databaseIN.surName}?${databaseIN.lastName}?${databaseIN.vehicleCode}?${databaseIN.km}?${databaseIN.transactionID}?${vehicleDescription}`;
+    let url = `${URLDOMAIN}?${databaseIN.firstName}?${databaseIN.lastName}?${databaseIN.vehicleCode}?${databaseIN.km}?${databaseIN.transactionID}?${vehicleDescription}`;
     return url;
 }
 
